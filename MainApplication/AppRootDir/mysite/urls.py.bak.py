2249b35a48adec376ebd2562ea363ba0a4ad59f1
Evan <Evan@v1040-wn-rt-c-4-151.campus-dynamic.uwaterloo.ca>
2020-01-28 12:27:40 -0500

    Update to Django v3.0.2

--- MainApplication/AppRootDir/mysite/urls.py.bak.py
+++ MainApplication/AppRootDir/mysite/urls.py.bak.py
@@ -1,7 +1,7 @@
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,15 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
-from django.conf.urls import url
 from django.contrib import admin
-from login import views
+from django.urls import path
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^index/', views.index),
-    url(r'^login/', views.login),
-    url(r'^register/', views.register),
-    url(r'^logout/', views.logout),
+    path('admin/', admin.site.urls),
 ]
