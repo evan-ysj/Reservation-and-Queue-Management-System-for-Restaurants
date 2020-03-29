f7f9e15db32de444899de2307f836735fd358ba6
Su Jia Yin <sjyin@uwaterloo.ca>
2020-03-29 11:11:25 -0400

    Update stored_procedure.sql

--- MainApplication/AppRootDir/stored_procedure.sql
+++ MainApplication/AppRootDir/stored_procedure.sql
@@ -7,6 +7,9 @@ drop procedure IF EXISTS occupation_update;
 DELIMITER $$
 CREATE PROCEDURE  occupation_update()
 BEGIN
+
+    UPDATE reservation_table SET reservation_table.occupied = FALSE  ;
+    
     UPDATE reservation_table, reservation_reservation
     SET reservation_table.occupied = TRUE
     WHERE reservation_table.table_id = reservation_reservation.table_id_id and reservation_reservation.date=CURDATE()  ;
